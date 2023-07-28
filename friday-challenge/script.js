d0fed07890e1311341f4f3f2b145db9235e75132
ruta-is-coding <rutajurg.123@gmail.com>
2023-07-28 09:44:55 +0300

    edited

---
+++ friday-challenge/script.js
@@ -0,0 +1,59 @@
+// 1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
+function compare(a, b) {
+  if (a > b) console.log(`Didesnis skaičius yra ${a}`);
+  else if (a === b) console.log("Skaičiai yra lygūs");
+  else console.log(`Didesnis skaičius yra ${b}`);
+}
+// 2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
+function numbersLine() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+// 3. Naudojant for ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10. (5 taškai)
+function evenNumbersLine() {
+  for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 0) console.log(i);
+  }
+}
+// 4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
+function rand(min, max) {
+  //funkcija atsitiktinio skaičiaus generavimui
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function fiveRandomNumbers() {
+  for (let i = 0; i < 5; i++) {
+    let number = rand(1, 10);
+    console.log(number);
+  }
+}
+// 5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
+function randomNumbers() {
+  while (true) {
+    let number = rand(1, 10);
+    console.log(number);
+    if (number === 5) break;
+  }
+}
+// 6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
+function bigArray() {
+  const array = [];
+  let max = 0;
+  let arrayLength = rand(20, 30);
+  for (let i = 0; i < arrayLength; i++) {
+    let number = rand(10, 30);
+    array.push(number);
+    if (number > max) max = number;
+  }
+  console.log(array);
+  console.log(`Maksimumas: ${max}`);
+}
+// 7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
+// 8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
+// Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei abu kintamieji skaičiai arba masyvai, bet suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)
+// 9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis
+// skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
+// 10. Parašyti funkciją telefonoNumeris. Funkcija turi priimti vieną kintamąjį - masyvą. Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu -
+// "(XXX) XXX-XXXX". (10 taškų)
