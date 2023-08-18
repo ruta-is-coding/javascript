587525331f78f927c983c926e7767890f6688dbd
ruta-is-coding <rutajurg.123@gmail.com>
2023-08-18 10:31:36 +0300

    edited errors

--- react/video-grotuvai/src/components/video.js
+++ react/video-grotuvai/src/components/video.js
@@ -12,9 +12,15 @@ const Video = ({
   let videoUrl = `https://www.youtube.com/embed/${id}?playlist=${id}&autoplay=${autoplay}&allow=${allow}&color=${color}&controls=${controls}&loop=${loop}&start=${start}&end=${end}`;
   let photoUrl = "https://picsum.photos/560/315";
   return id === "" || thumbnail ? (
-    <img src={photoUrl} alt="Random Photo" className="mb-5" />
+    <img src={photoUrl} alt="" className="mb-5" />
   ) : (
-    <iframe src={videoUrl} width={560} height={315} className="mb-5"></iframe>
+    <iframe
+      src={videoUrl}
+      width={560}
+      height={315}
+      className="mb-5"
+      title="videoPlayer"
+    ></iframe>
   );
 };
 
