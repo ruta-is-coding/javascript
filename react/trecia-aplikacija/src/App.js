73cd475b40684049f81353ff7484155db21406f3
ruta-is-coding <rutajurg.123@gmail.com>
2023-08-21 12:43:22 +0300

    edited

--- react/trecia-aplikacija/src/App.js
+++ react/trecia-aplikacija/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [text, setText] = useState("Tekstas");
+
+  return (
+    <div className="container">
+      <h2>Teksto atvaizdavimas</h2>
+      <form className="mt-3">
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="mt-2">{text}</div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
