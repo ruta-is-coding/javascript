9bda101f3b01549907055607719108d9a35317f7
ruta-is-coding <rutajurg.123@gmail.com>
2023-08-29 21:49:05 +0300

    edited

--- react/food-search/src/pages/FirstLetter.js
+++ react/food-search/src/pages/FirstLetter.js
@@ -1,12 +1,34 @@
+import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
 const FirstLetter = () => {
+  const [data, setData] = useState([]);
   const { letter } = useParams();
 
-  const alphabet = [];
-  for (let i = 0; i < 26; i++) alphabet.push(String.fromCharCode(65 + i));
+  useEffect(() => {
+    fetch("https:/www.themealdb.com/api/json/v1/1/search.php?f=" + letter)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setData(resp.meals);
+        console.log(resp);
+      });
+  }, []);
 
-  return <h1>Filter by letter {letter}</h1>;
+  return (
+    <>
+      <h1>Filter by letter {letter}</h1>
+      <div className="row mt-3">
+        {data.map((value) => (
+          <div className="col-6 mb-3" key={value.idMeal}>
+            <Link to={"/meal/" + value.idMeal}>
+              <img src={value.strMealThumb} alt={value.strMeal} />
+            </Link>
+            <h3>{value.strMeal}</h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default FirstLetter;
--- react/food-search/src/pages/Home.js
+++ react/food-search/src/pages/Home.js
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const alphabet = [];
+  for (let i = 0; i < 26; i++) {
+    alphabet.push(String.fromCharCode(65 + i));
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,6 +18,18 @@ const Home = () => {
       .then((resp) => resp.json())
       .then((resp) => setData(resp.meals));
   };
+
+  //Kaip iš select option ištraukti vertę?
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setCategories(resp.categories);
+      });
+  }, []);
+
+  console.log(categories);
 
   return (
     <>
@@ -29,6 +47,26 @@ const Home = () => {
         <Link to={"/random"} className="btn btn-success">
           I'm lucky
         </Link>
+      </div>
+      <div className="d-flex gap-3 mt-2 justify-content-center flex-wrap">
+        {alphabet.map((letter, index) => (
+          <Link key={index} to={"/letter/" + letter}>
+            {letter}
+          </Link>
+        ))}
+      </div>
+      <div className="col-3">
+        <select className="my-3 form-select">
+          {categories.map((value, index) => (
+            <option
+              value={value.strCategory}
+              key={index}
+              // onChange={categoryItems}
+            >
+              {value.strCategory}
+            </option>
+          ))}
+        </select>
       </div>
       <div className="row mt-3">
         {data.map((value) => (
