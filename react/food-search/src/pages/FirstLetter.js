ca6ec31c5cc028c64064ae7a446e5236a8058d1c
ruta-is-coding <rutajurg.123@gmail.com>
2023-08-29 13:56:38 +0300

    edited

--- react/food-search/src/pages/FirstLetter.js
+++ react/food-search/src/pages/FirstLetter.js
@@ -3,6 +3,9 @@ import { useParams, Link } from "react-router-dom";
 const FirstLetter = () => {
   const { letter } = useParams();
 
+  const alphabet = [];
+  for (let i = 0; i < 26; i++) alphabet.push(String.fromCharCode(65 + i));
+
   return <h1>Filter by letter {letter}</h1>;
 };
 
